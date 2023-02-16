951ce8b16260919d0accaa8a1ac25398be4cf501
Jude Tochukwu Ogbozor <judtheory@gmail.com>
2023-02-16 07:27:57 -0800

    c if fun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+/**
+* main - prints the phrase "Programming is like building a multilingual puzzle
+* Return: 0 if exited properly, non-zero otherwise
+*/
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
