b23deafd391e94f7589f15d0477107fd40c4cb49
Jude Tochukwu Ogbozor <judtheory@gmail.com>
2023-02-17 12:01:30 -0800

    adding comment

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more header goes there*/
-/* betty style doc for function main goes there*/
+
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
